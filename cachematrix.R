022d027b96a709b8f1fc66c1ea475dd7ad9ee15c
Kishore Reddy L <lkishore.reddy@gmail.com>
2015-09-28 00:17:59 +0530

    Programming assignment 2: Create inverse of a matrix using Cache

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## The function will output the inverse the given matrix.
+## Initially it will cache the inverse. The same cache will be used in cache solve function.
+## when the function is run again, the cache will be flushed out and inverse will be calculated again.
+makeCacheMatrix <- function(x = numeric()) {
+  cache <- NULL
+  setMatrix <- function(y) {
+    x <<- y
+    cache <<- NULL
+  }
+  getMatrix <- function()  x
+  
+  cacheInverse <- function(solve)   cache <<- solve
+  
+  getInverse <- function()    cache
+  
+  list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
+## Check the inverse matrix and if is not null, then return the inverse matrix
+## Calculate the inverse of matrix if it is null.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(y, ...) {
+  inverse <- y$getInverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- y$getMatrix()
+  inverse <- solve(data)
+  y$cacheInverse(inverse)
+  inverse
 }
